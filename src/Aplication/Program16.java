cd0aa8617eb9264fdb66d0dc89d0c3b1ed2ebc20
MatheusSousa98 <matheusousams.98@gmail.com>
2022-08-09 10:13:43 -0300

    teste

---
+++ src/Aplication/Program16.java
@@ -0,0 +1,50 @@
+package Aplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Entities.Product;
+
+public class Program16 {
+	
+	public static int compareProducts(Product p1, Product p2) {
+		
+		return p1.getPrice().compareTo(p2.getPrice());
+	}
+
+	public static void main(String[] args) {
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+		list.sort(Program16 :: compareProducts);
+		list.forEach(System.out::println);
+		
+	}
+}
+
+
+/*package Aplication;
+
+import java.util.Arrays;
+
+public class Program16 {
+	
+	public static int globalValue = 3;
+
+	public static void main(String[] args) {
+		
+		int[] vect = new int[] { 3, 4, 5 };
+		changeOddValues(vect);
+		System.out.println(Arrays.toString(vect));
+	}
+
+	public static void changeOddValues(int[] numbers) {
+		for (int i = 0; i < numbers.length; i++) {
+			if (numbers[i] % 2 != 0) {
+				numbers[i] += globalValue;
+			}
+		}
+	}
+}*/
