9b4bac247660933eb39e0fdb4cebac5113e75c0a
Icaro <100494049+Icaroiz@users.noreply.github.com>
2022-08-20 08:16:27 -0300

    exercicio

---
+++ src/Aplication/Program19.java
@@ -0,0 +1,58 @@
+package Aplication;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import Entities.Product;
+
+public class Program19 {
+
+	public static void main (String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Entre com o caminho do arquivo: ");
+		String path = sc.nextLine();
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(path))){
+			
+			List<Product> list = new ArrayList<>();
+			
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(",");
+				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+				line = br.readLine();
+			}
+			
+			double avg = list.stream()
+					.map(p -> p.getPrice())
+					.reduce(0.0, (x,y) -> x + y) / list.size();
+			
+			System.out.println("Somatória dos preços " + String.format("%.2f", avg));
+			
+			Comparator<String> comp = (s1,s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+			
+			List <String> names = list.stream()
+					.filter(p -> p.getPrice() < avg)
+					.map(p -> p.getName()).sorted(comp.reversed())
+					.collect(Collectors.toList());
+			
+			names.forEach(System.out :: println);
+			
+		}catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+			
+		}
+		sc.close();
+	}
+
+}
